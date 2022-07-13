536708548c00ff5e2b6c85202921597d232907f3
Starsakary <starsakary@gmail.com>
2022-07-13 18:06:23 +0800

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'JustLend',
+    description: 'JustLend Documentation',
+    base: '/doc/',
+    theme: '',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            label: 'English',
+            title: 'JustLend Documentation',
+            description: 'JustLend Documentation'
+        }
+    },
+    themeConfig: {
+        locales: {
+            '/' : {
+                selectText: 'Language',
+                label: 'English',
+                sidebar : {
+                    '/': [
+                        ''
+                    ]
+                }
+            }
+        }
+    }
+}
